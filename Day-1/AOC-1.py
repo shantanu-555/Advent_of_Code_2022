c99f84016eee6a0db39502c540b106bb2447e8a0
Shantanu Motiani <shantanumotiani@gmail.com>
2023-01-24 18:55:59 +0100

    Day 1 Done

--- Day-1/AOC-1.py
+++ Day-1/AOC-1.py
@@ -3,18 +3,21 @@ with open('input1.in') as file:
     
 # print(data)
 
-''' Traversing every string in our file '''
+''' Traversing every string in data and appending to lst '''
 count = 0
 lst = []
 
 for item in data:
-    if item == '':
-        lst.append(count)
-        count = 0
-    else:
-        num = int(item)
-        count += num
+    if item == '':          # Before new elf begins:
+        lst.append(count)   # Append previous elf's calories
+        count = 0           # Reset count
         
-print(f"Answer to part 1 is {sorted(lst)[-1]}")
+    else:                   # For the same elf:
+        num = int(item)         
+        count += num        # Sum calories
+
+sorted_lst = sorted(lst)    # Sort lst in ascending order
+
+print(f"Answer to part 1 is {sorted_lst[-1]}")
 print(f"Answer to part 2 is \
-{sorted(lst)[-1] + sorted(lst)[-2] + sorted(lst)[-3]}")
+{sorted_lst[-1] + sorted_lst[-2] + sorted_lst[-3]}")
